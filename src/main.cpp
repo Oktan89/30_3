36659dbc0565ea288cb7c08ff449bfb90b9bae84
-=Oktan=- <r.zhigalenko@mail.ru>
2021-09-28 00:46:25 +0500

    post pair

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include <vector>
 #include <cpr/cpr.h>
 
 int main()
 {
-    std::cout << "Test" <<std::endl;
+    std::string query = "post";
+    cpr::Url url = cpr::Url{"http://httpbin.org/"+ query};
+
+    std::vector<cpr::Pair> vec;
+    vec.push_back({"zorg", "red"});
+    vec.push_back({"dog", "black"});
+    vec.push_back({"cat", "black"});
+    vec.push_back({"cat2", "blue"});
+     
+    
+    cpr::Payload p(vec.begin(), vec.end());
+    
+    auto r = cpr::Post(url, p);
+    std::cout << r.text <<std::endl;
 }
