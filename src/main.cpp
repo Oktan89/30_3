25a7d1d320f58cf5ef791906650193f56e4305d7
-=Oktan=- <r.zhigalenko@mail.ru>
2021-09-29 23:20:04 +0500

    completed

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,66 @@
 #include <vector>
 #include <cpr/cpr.h>
 
+
+bool request(const std::string& query, const std::vector<cpr::Pair> &vec)
+{
+
+    std::cout << "We send a request to the website, please wait..." << std::endl;
+    if(query == "post")
+    {
+        cpr::Url url = cpr::Url{"http://httpbin.org/"+ query}; 
+        cpr::Payload p(vec.begin(), vec.end());
+        auto r = cpr::Post(url, p);
+        std::cout << r.text <<std::endl;
+    }
+    else
+    {
+        std::string arg = "?";
+        for(int i = 0; i < vec.size();)
+        {
+            arg += vec[i].key + "=" + vec[i].value;
+            ++i;
+            if(i != vec.size())
+               arg += "&";
+        }
+            
+        cpr::Url url = cpr::Url{"http://httpbin.org/"+ query + arg}; 
+        auto r = cpr::Get(url);
+        std::cout << r.text <<std::endl;
+    }
+    return true;
+}
+
 int main()
 {
-    std::string query = "post";
-    cpr::Url url = cpr::Url{"http://httpbin.org/"+ query};
 
+
+    
+    std::cout << "enter arguments and a post or get for the request: " << std::endl;
+    std::string argf = "", args = "";
+    bool exit = false;
     std::vector<cpr::Pair> vec;
-    vec.push_back({"zorg", "red"});
-    vec.push_back({"dog", "black"});
-    vec.push_back({"cat", "black"});
-    vec.push_back({"cat2", "blue"});
-     
-    
-    cpr::Payload p(vec.begin(), vec.end());
-    
-    auto r = cpr::Post(url, p);
-    std::cout << r.text <<std::endl;
+    do
+    {
+        std::cout << "Enter first argument: ";
+        std::cin >> argf;
+        if(argf != "post" && argf != "get")
+        {
+            std::cout << "Enter second argument: ";
+            std::cin >> args;
+            if(args != "post" && args != "get")
+            {
+                vec.push_back({argf, args});
+            }
+            else
+            {
+                exit = request(args, vec);
+            }
+        }
+        else
+        {
+            exit = request(argf, vec);
+        }
+    } while (!exit);
+
 }
